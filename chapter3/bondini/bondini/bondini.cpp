ceec2dbb235e8e89e2a616b10c180e80650a7377
VincentQin <vincent.qin_1@signify.com>
2018-12-27 15:47:58 +0800

    add code 3.7 bondini.cpp

---
+++ chapter3/bondini/bondini/bondini.cpp
@@ -0,0 +1,23 @@
+// bondini.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	using namespace std;
+	cout << "\aOperation \"HyperHype\" is now activated!\n";
+	cout << "Enter you agent code:____\b\b\b\b\b\b\b\b";
+	long code;
+
+	cin >> code;
+	cout << "\aYou enter " << code << "...\n";
+	cout << "\aCode verified! Proceed with Plan Z3!";
+
+
+	system("pause");
+	return 0;
+}
+
