92e8ed9173815db3917023275569153d598c032f
VincentQin <vincent.qin_1@signify.com>
2018-12-28 10:36:13 +0800

    add code 3.10 arith.cpp

---
+++ chapter3/arith/arith/arith.cpp
@@ -0,0 +1,26 @@
+// arith.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	using namespace std;
+	float hats, heads;
+	cout.setf(ios_base::fixed, ios_base::floatfield);//fixed-point
+	cout << "Enter a number: ";
+	cin >> hats;
+	cout << "Enter another number: ";
+	cin >> heads;
+
+	cout << "hats = " << hats << "; heads = " << heads << endl;
+	cout << "hats + heads = " << hats + heads << endl;
+	cout << "hats - heads = " << hats - heads << endl;
+	cout << "hats * heads = " << hats * heads << endl;
+	cout << "hats / heads = " << hats / heads << endl;
+	
+	system("pause");
+	return 0;
+}
+
