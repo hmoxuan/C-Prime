74f95cfdcce3189c545ecc1d6571f757c4aba228
VincentQin <vincent.qin_1@signify.com>
2018-12-28 11:07:33 +0800

    add code 3.14 typecast.cpp

---
+++ chapter3/typecast/typecast/typecast.cpp
@@ -0,0 +1,30 @@
+// typecast.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	using namespace std;
+	int auks, bats, coots;
+
+	// the following statement adds the values as double,
+	// then converts the result to int
+	auks = 19.99 + 11.99;
+
+	//these statements add values as int
+	bats = (int) 19.99 + (int) 11.99;  //old C syntax
+	coots = int (19.99) + int (11.99);  //New C++ syntax
+	cout << "auks = " << auks << ", bats = " << bats;
+	cout << ", coots = " << coots << endl;
+
+    char ch = 'Z';
+	cout << "The code for " << ch << " is ";  // print as char
+	cout << int(ch) <<endl;
+	cout << "Yes, the code is ";
+	cout << static_cast<int>(ch) << endl;  // using static_cast
+
+	system("pause");
+	return 0;
+}
