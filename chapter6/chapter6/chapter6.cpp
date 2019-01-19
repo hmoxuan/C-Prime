5ca69658d3c345875da6a994f4cd3fd849d5c7eb
VincentQin <vincent.qin_1@signify.com>
2019-01-20 00:02:47 +0800

    add code 6.1 and 6.2

--- chapter6/chapter6/chapter6.cpp
+++ chapter6/chapter6/chapter6.cpp
@@ -25,12 +25,35 @@ void if_func(void)
 	cout << " characters total in sentence" << endl;
 }
 
+// code 6.2. ifelse -- using the if else statement
+void ifelse(void)
+{
+	using namespace std;
+	char ch;
+    cout << "Type, and I shall repeat." << endl;
+	cin.get(ch);
+
+	while( ch!= '.') // quit at end of sentence
+	{
+		if (ch == '\n') // check if ch is a space
+		{
+			cout << ch; 
+		}
+		else
+		{
+			cout << ++ch;
+		}
+		cin.get(ch);
+	}
+	cout << " \n Please excuse the slight confusion." << endl;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	using namespace std;
 	cout << "chapter6" << endl;
-	if_func();
+	//if_func();
+	ifelse();
 
 	system("pause");
 	return 0;
