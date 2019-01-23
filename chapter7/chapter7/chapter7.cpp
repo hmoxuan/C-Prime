7316eb69125f8e733e6f974e1a14b1423f0a2161
VincentQin <vincent.qin_1@signify.com>
2019-01-23 22:42:51 +0800

    add code 7.11

--- chapter7/chapter7/chapter7.cpp
+++ chapter7/chapter7/chapter7.cpp
@@ -359,6 +359,47 @@ void strgback(void)
 	delete [] ps;  // free memory
 }
 
+/******************************************************
+ * code 7.11 travel -- using structure with functions
+ *
+ /****************************************************/
+struct travel_time
+{
+	int hours;
+	int mins;
+};
+
+const int Mins_per_hr = 60;
+travel_time sum(travel_time t1, travel_time t2)
+{
+	travel_time total;
+	total.mins = (t1.mins + t2.mins) % Mins_per_hr;
+	total.hours = t1.hours + t2.hours + (t1.hours + t2.hours) / Mins_per_hr;
+	return total;
+}
+
+void show_time(travel_time t)
+{
+	using namespace std;
+	cout << t.hours << " hours, " << t.mins << " minutes\n";
+
+}
+
+void travel(void)
+{
+	using namespace std;
+	travel_time day1 = {5, 45};
+	travel_time day2 = {4, 55};
+
+	travel_time trip = sum(day1, day2);
+	cout << "Two-day total: ";
+	show_time(trip);
+
+	travel_time day3 = {4, 32};
+	cout << "Three-day total: ";
+	show_time( sum(trip, day3) );
+}
+
 
 /******************************************************
  * main function
@@ -376,7 +417,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	//arrfun3();
 	//arrfun4();
 	//strgfun();
-	strgback();
+	//strgback();
+	travel();
 
 	system("pause");
 	return 0;
