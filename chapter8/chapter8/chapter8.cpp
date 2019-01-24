0c85d1b4b1cd19fc16e38136d60ce3c0bf71bd20
VincentQin <vincent.qin_1@signify.com>
2019-01-24 23:19:58 +0800

    add code 8.1-8.3

--- chapter8/chapter8/chapter8.cpp
+++ chapter8/chapter8/chapter8.cpp
@@ -3,10 +3,64 @@
 #include "stdafx.h"
 #include <iostream>
 
+// 8.1 -- san inline function definition
+inline double square(double x) { return x * x;}
+void inlineFunc(void)
+{
+	using namespace std;
+	double a, b;
+	double c = 13.0;
+
+	a = square(5.0);
+	b = square(4.5 + 7.5);
+	cout << "a = " << a << ", b = " << b << endl;
+	cout << "c = " << c;
+	cout << ", c squared = " << square(c++) << endl;
+	cout << "Now c = " << c << endl;
+}
+
+// 8.2 firstref -- defining and using a reference
+void firstref(void)
+{
+	using namespace std;
+	int rats = 101;
+	int & rodents = rats;
+	cout << "rats = " << rats;
+	cout << ", rodents = " << rodents << endl;
+	rodents++;
+	cout << "rats = " << rats;
+	cout << ", rodents = " << rodents << endl;
+}
+
+// 8.3 sceref -- defining and using a reference
+void sceref(void)
+{
+	using namespace std;
+	int rats = 101;
+	int & rodents = rats;
+	cout << "rats = " << rats;
+	cout << ", rodents = " << rodents << endl;
+
+	cout << "rats address = " << &rats;
+	cout << ", rodents address = " << &rodents << endl;
+
+	int bunnies = 50;
+	rodents = bunnies;
+	cout << "bunnies = " << bunnies;
+	cout << ", rats = " << rats;
+	cout << ", rodents = " << rodents << endl;
+
+	cout << "bunnies address = " << &bunnies;
+	cout << ", rodents address = " << &rodents << endl;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	using namespace std;
 	cout << "chapter8" << endl;
+	//inlineFunc();
+	//firstref();
+	sceref();
 
 	system("pause");
 	return 0;
